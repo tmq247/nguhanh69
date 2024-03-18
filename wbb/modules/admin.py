a8d43d3af329ae61be94d15314d3a359ca798ea6
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-03-19 01:16:01 +0700

    Update admin.py

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -460,6 +460,9 @@ async def deleteFunc(_, message: Message):
         return await message.reply_text("không tìm thấy người này")
     served_chats = await get_served_chats()
     number_of_chats = 0
+    await message.reply_text(
+        f"**Đang xóa trong khoảng {len(served_chats)} giây.**"
+    )
     for served_chat in served_chats:
         try:
             await app2.delete_user_history(served_chat["chat_id"], user_id)
