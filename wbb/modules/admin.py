155f8f8e5d3b6e5391ad1f0f0f3996ed20a2b5ea
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-03-19 20:04:04 +0700

    Update admin.py

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -453,8 +453,8 @@ async def list_unban_(c, message: Message):
 @app.on_message(filters.command(["d", "del"]) & ~filters.private)
 @adminsOnly("can_delete_messages")
 async def deleteFunc(_, message: Message):
-    user_id = await extract_user(message)
-    user = await app.get_users(user_id)
+    user_id = await extract_user(message)#
+    user = await app.get_users(user_id)#
     from_user = message.from_user#
     if not user_id: #message.reply_to_message:
         return await message.reply_text("không tìm thấy người này")
@@ -462,7 +462,7 @@ async def deleteFunc(_, message: Message):
     number_of_chats = 0
     for served_chat in served_chats:
         try:
-            await app2.delete_user_history(served_chat["chat_id"], user_id)
+            await app2.delete_user_history(served_chat["chat_id"], user.id)
             number_of_chats += 1
             await asyncio.sleep(1)
         except FloodWait as e:
