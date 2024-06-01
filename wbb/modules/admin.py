38f2354113e7f0a2c5fc192d4cb94e7b479b2678
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-06-02 00:13:26 +0700

    Update admin.py

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -463,7 +463,7 @@ async def list_unban_(c, message: Message):
 @adminsOnly("can_delete_messages")
 async def deleteFunc(_, message: Message):
     user_id = await extract_user(message)#
-    await app2.message.delete()
+    await app2.message_delete()
     served_chats = await get_served_chats()
     #user = await app.get_users(user_id)#
     #from_user = message.from_user#
