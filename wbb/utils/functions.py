e2ace29e38c6459d04c5c5d7ab0d9915858ec1de
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-07-12 15:48:14 +0700

    Update functions.py

--- wbb/utils/functions.py
+++ wbb/utils/functions.py
@@ -400,14 +400,3 @@ async def get_user_id_and_usernames(client) -> dict:
         users_[user_id] = username
 
     return users_
-
-def get_peer_type_new(peer_id: int) -> str:
-    peer_id_str = str(peer_id)
-    if not peer_id_str.startswith("-"):
-        return "user"
-    elif peer_id_str.startswith("-100"):
-        return "channel"
-    else:
-        return "chat"
-
-utils.get_peer_type = get_peer_type_new
