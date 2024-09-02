c50f1efb006458f0b3d5aa1bf73eb1c6a7d4d42f
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-09-02 08:02:11 +0700

    Update admin.py

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -960,6 +960,7 @@ __**Người dùng bị cấm chat toàn hệ thống**__
             text=mute_text,
             disable_web_page_preview=True,
         )
+        await app.send_mesage(FMUTE_LOG_GROUP_ID,f"<a href='tg://user?id={msg.from_user.id}'>{msg.from_user.full_name}</a>", "HTML")
         await m.edit(
 f"""**🔥Người dùng {user.mention} @{username2} đã bị 🚫cấm chat tất cả nhóm trong hệ thống.**
 **Bởi: {from_user.mention} @{username1}.**
