a7bcd9be597e8036192f74b90ec2de0e1306b6f0
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-09-06 15:48:51 +0700

    Update sudoers.py

--- wbb/modules/sudoers.py
+++ wbb/modules/sudoers.py
@@ -240,7 +240,7 @@ __**Người dùng được bỏ chặn**__
 @capture_err
 async def broadcast_message(_, message):
     sleep_time = 0.1
-    text = message.reply_to_message.text.markdown
+    text = message.reply_to_message.text
     reply_message = message.reply_to_message
 
     reply_markup = None
