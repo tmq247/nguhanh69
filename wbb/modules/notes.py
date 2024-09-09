7805d9c2ee9f25c9c86275c98bf8e739ebf4082f
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-09-10 01:52:23 +0700

    Update notes.py

--- wbb/modules/notes.py
+++ wbb/modules/notes.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2023 TheHamkerCat
+Copyright (c) 2024 TheHamkerCat
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,65 +21,139 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
 from re import findall
 
 from pyrogram import filters
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 
 from wbb import SUDOERS, USERBOT_ID, USERBOT_PREFIX, app, app2, eor
 from wbb.core.decorators.errors import capture_err
 from wbb.core.decorators.permissions import adminsOnly
 from wbb.core.keyboard import ikb
-from wbb.utils.dbfunctions import delete_note, get_note, get_note_names, save_note
-from wbb.utils.functions import extract_text_and_keyb
+from wbb.modules.admin import member_permissions
+from wbb.utils.dbfunctions import (
+    delete_note,
+    deleteall_notes,
+    get_note,
+    get_note_names,
+    save_note,
+)
+from wbb.utils.functions import (
+    check_format,
+    extract_text_and_keyb,
+    get_data_and_name,
+)
 
 __MODULE__ = "Notes"
 __HELP__ = """/notes To Get All The Notes In The Chat.
 
-/save [NOTE_NAME] To Save A Note (Can be a sticker or text).
+/save [NOTE_NAME] To Save A Note.
+
+Supported note types are Text, Animation, Photo, Document, Video, video notes, Audio, Voice.
+
+To change caption of any files use.\n/save [NOTE_NAME] [NEW_CAPTION].
 
 #NOTE_NAME To Get A Note.
 
 /delete [NOTE_NAME] To Delete A Note.
+/deleteall To delete all the notes in a chat (permanently).
 
 Checkout /markdownhelp to know more about formattings and other syntax.
 """
 
 
-@app2.on_message(
-    filters.command("save", prefixes=USERBOT_PREFIX)
-    & ~filters.forwarded
-    & ~filters.via_bot
-    & SUDOERS
-)
+def extract_urls(reply_markup):
+    urls = []
+    if reply_markup.inline_keyboard:
+        buttons = reply_markup.inline_keyboard
+        for i, row in enumerate(buttons):
+            for j, button in enumerate(row):
+                if button.url:
+                    name = (
+                        "\n~\nbutton"
+                        if i * len(row) + j + 1 == 1
+                        else f"button{i * len(row) + j + 1}"
+                    )
+                    urls.append((f"{name}", button.text, button.url))
+    return urls
+
+
+@app2.on_message(filters.command("save", prefixes=USERBOT_PREFIX) & SUDOERS & ~filters.via_bot)
 @app.on_message(filters.command("save") & ~filters.private)
 @adminsOnly("can_change_info")
 async def save_notee(_, message):
-    if len(message.command) < 2 or not message.reply_to_message:
-        await eor(
-            message,
-            text="**Usage:**\nReply to a text or sticker with /save [NOTE_NAME] to save it.",
+    try:
+        if len(message.command) < 2:
+            await eor(
+                message,
+                text="**Usage:**\nReply to a message with /save [NOTE_NAME] to save a new note.",
+            )
+        else:
+            replied_message = message.reply_to_message
+            if not replied_message:
+                replied_message = message
+            data, name = await get_data_and_name(replied_message, message)
+            if data == "error":
+                return await message.reply_text(
+                    "**Usage:**\n__/save [NOTE_NAME] [CONTENT]__\n`-----------OR-----------`\nReply to a message with.\n/save [NOTE_NAME]"
+                )
+            if replied_message.text:
+                _type = "text"
+                file_id = None
+            if replied_message.sticker:
+                _type = "sticker"
+                file_id = replied_message.sticker.file_id
+            if replied_message.animation:
+                _type = "animation"
+                file_id = replied_message.animation.file_id
+            if replied_message.photo:
+                _type = "photo"
+                file_id = replied_message.photo.file_id
+            if replied_message.document:
+                _type = "document"
+                file_id = replied_message.document.file_id
+            if replied_message.video:
+                _type = "video"
+                file_id = replied_message.video.file_id
+            if replied_message.video_note:
+                _type = "video_note"
+                file_id = replied_message.video_note.file_id
+            if replied_message.audio:
+                _type = "audio"
+                file_id = replied_message.audio.file_id
+            if replied_message.voice:
+                _type = "voice"
+                file_id = replied_message.voice.file_id
+            if replied_message.reply_markup and not findall(r"\[.+\,.+\]", data):
+                urls = extract_urls(replied_message.reply_markup)
+                if urls:
+                    response = "\n".join(
+                        [f"{name}=[{text}, {url}]" for name, text, url in urls]
+                    )
+                    data = data + response
+            if data:
+                data = await check_format(ikb, data)
+                if not data:
+                    return await message.reply_text(
+                        "**Wrong formatting, check the help section.**"
+                    )
+            note = {
+                "type": _type,
+                "data": data,
+                "file_id": file_id,
+            }
+            chat_id = USERBOT_ID if message.text.startswith(USERBOT_PREFIX) else message.chat.id
+            await save_note(chat_id, name, note)
+            await eor(message, text=f"__**Saved note {name}.**__")
+    except UnboundLocalError:
+        return await message.reply_text(
+            "**Replied message is inaccessible.\n`Forward the message and try again`**"
         )
-
-    elif not message.reply_to_message.text and not message.reply_to_message.sticker:
-        await eor(
-            message,
-            text="__**You can only save text or stickers in notes.**__",
-        )
-    else:
-        name = message.text.split(None, 1)[1].strip()
-        if not name:
-            return await eor(message, text="**Usage**\n__/save [NOTE_NAME]__")
-        _type = "text" if message.reply_to_message.text else "sticker"
-        note = {
-            "type": _type,
-            "data": message.reply_to_message.text.markdown
-            if _type == "text"
-            else message.reply_to_message.sticker.file_id,
-        }
-        prefix = message.text.split()[0][0]
-        chat_id = message.chat.id if prefix != USERBOT_PREFIX else USERBOT_ID
-        await save_note(chat_id, name, note)
-        await eor(message, text=f"__**Saved note {name}.**__")
 
 
 @app2.on_message(
@@ -122,40 +196,98 @@ async def get_one_note_userbot(_, message):
     if not _note:
         return await eor(message, text="No such note.")
 
-    if _note["type"] == "text":
-        data = _note["data"]
-        await eor(
-            message,
-            text=data,
-            disable_web_page_preview=True,
-        )
-    else:
-        await message.reply_sticker(_note["data"])
+    type = _note["type"]
+    data = _note["data"]
+    file_id = _note.get("file_id")
+    keyb = None
+    await get_reply(message, type, file_id, data, keyb, protect)
 
 
 @app.on_message(filters.regex(r"^#.+") & filters.text & ~filters.private)
 @capture_err
 async def get_one_note(_, message):
+    from_user = message.from_user if message.from_user else message.sender_chat
+    chat_id = message.chat.id
     name = message.text.replace("#", "", 1)
     if not name:
         return
-    _note = await get_note(message.chat.id, name)
+    _note = await get_note(chat_id, name)
     if not _note:
         return
-    if _note["type"] == "text":
-        data = _note["data"]
-        keyb = None
+    type = _note["type"]
+    data = _note["data"]
+    file_id = _note.get("file_id")
+    keyb = None
+    if data:
+        if "{chat}" in data:
+            data = data.replace("{chat}", message.chat.title)
+        if "{name}" in data:
+            data = data.replace(
+                "{name}", (from_user.mention if message.from_user else from_user.title)
+            )
         if findall(r"\[.+\,.+\]", data):
             keyboard = extract_text_and_keyb(ikb, data)
             if keyboard:
                 data, keyb = keyboard
+    replied_message = message.reply_to_message
+    if replied_message:
+        replied_user = replied_message.from_user if replied_message.from_user else replied_message.sender_chat
+        if replied_user.id != from_user.id:
+            message = replied_message
+    await get_reply(message, type, file_id, data, keyb)
+
+
+async def get_reply(message, type, file_id, data, keyb):
+    if type == "text":
         await message.reply_text(
-            data,
+            text=data,
             reply_markup=keyb,
             disable_web_page_preview=True,
         )
-    else:
-        await message.reply_sticker(_note["data"])
+    if type == "sticker":
+        await message.reply_sticker(
+            sticker=file_id,
+        )
+    if type == "animation":
+        await message.reply_animation(
+            animation=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "photo":
+        await message.reply_photo(
+            photo=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "document":
+        await message.reply_document(
+            document=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "video":
+        await message.reply_video(
+            video=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "video_note":
+        await message.reply_video_note(
+            video_note=file_id,
+        )
+    if type == "audio":
+        await message.reply_audio(
+            audio=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
+    if type == "voice":
+        await message.reply_voice(
+            voice=file_id,
+            caption=data,
+            reply_markup=keyb,
+        )
 
 
 @app2.on_message(
@@ -182,3 +314,49 @@ async def del_note(_, message):
         await eor(message, text=f"**Deleted note {name} successfully.**")
     else:
         await eor(message, text="**No such note.**")
+
+
+@app.on_message(filters.command("deleteall") & ~filters.private)
+@adminsOnly("can_change_info")
+async def delete_all(_, message):
+    _notes = await get_note_names(message.chat.id)
+    if not _notes:
+        return await message.reply_text("**No notes in this chat.**")
+    else:
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "YES, DO IT", callback_data="delete_yes"
+                    ),
+                    InlineKeyboardButton("Cancel", callback_data="delete_no"),
+                ]
+            ]
+        )
+        await message.reply_text(
+            "**Are you sure you want to delete all the notes in this chat forever ?.**",
+            reply_markup=keyboard,
+        )
+
+
+@app.on_callback_query(filters.regex("delete_(.*)"))
+async def delete_all_cb(_, cb):
+    chat_id = cb.message.chat.id
+    from_user = cb.from_user
+    permissions = await member_permissions(chat_id, from_user.id)
+    permission = "can_change_info"
+    if permission not in permissions:
+        return await cb.answer(
+            f"You don't have the required permission.\n Permission: {permission}",
+            show_alert=True,
+        )
+    input = cb.data.split("_", 1)[1]
+    if input == "yes":
+        stoped_all = await deleteall_notes(chat_id)
+        if stoped_all:
+            return await cb.message.edit(
+                "**Successfully deleted all notes on this chat.**"
+            )
+    if input == "no":
+        await cb.message.reply_to_message.delete()
+        await cb.message.delete()
