fb6aeca3d92c61e587b3443478d5fe31b2eed95f
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-09-29 22:55:02 +0700

    Update info.py

--- wbb/modules/info.py
+++ wbb/modules/info.py
@@ -39,7 +39,7 @@ __HELP__ = """
 
 async def get_user_info(user, chat, already=False):
     if not already:
-        await app.get_chat_member(chat, user)
+        await app.get_chat_members(chat)
         user = await app2.get_users(user)
     if not user.first_name:
         return ["Deleted account", None]
