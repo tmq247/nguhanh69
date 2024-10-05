1a7003904746c36fc0d6814aef0f368ad2c8c85d
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-10-05 13:33:43 +0700

    Update filters.py

--- wbb/modules/filters.py
+++ wbb/modules/filters.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2023 TheHamkerCat
+Copyright (c) 2024 TheHamkerCat
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,28 +21,47 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
 import re
 
 from pyrogram import filters
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 
 from wbb import app
 from wbb.core.decorators.errors import capture_err
 from wbb.core.decorators.permissions import adminsOnly
 from wbb.core.keyboard import ikb
+from wbb.modules.admin import member_permissions
+from wbb.modules.notes import extract_urls
 from wbb.utils.dbfunctions import (
     delete_filter,
+    deleteall_filters,
     get_filter,
     get_filters_names,
     save_filter,
 )
 from wbb.utils.filter_groups import chat_filters_group
-from wbb.utils.functions import extract_text_and_keyb
+from wbb.utils.functions import (
+    check_format,
+    extract_text_and_keyb,
+    get_data_and_name,
+)
 
 __MODULE__ = "Filters"
 __HELP__ = """/filters To Get All The Filters In The Chat.
-/filter [FILTER_NAME] To Save A Filter (Can be a sticker or text).
-/stop [FILTER_NAME] To Stop A Filter.
+/filter [FILTER_NAME] To Save A Filter(reply to a message).
 
+Supported filter types are Text, Animation, Photo, Document, Video, video notes, Audio, Voice.
+
+To use more words in a filter use.
+`/filter Hey_there` To filter "Hey there".
+
+/stop [FILTER_NAME] To Stop A Filter.
+/stopall To delete all the filters in a chat (permanently).
 
 You can use markdown or html to save text too.
 
@@ -50,37 +69,75 @@ Checkout /markdownhelp to know more about formattings and other syntax.
 """
 
 
-@app.on_message(filters.command("fil") & ~filters.private)
+@app.on_message(filters.command("filter") & ~filters.private)
 @adminsOnly("can_change_info")
 async def save_filters(_, message):
-    if len(message.command) < 2 or not message.reply_to_message:
-        return await message.reply_text(
-            "**Usage:**\nReply to a text or sticker with /filter [FILTER_NAME] to save it."
-        )
-    if not message.reply_to_message.text and not message.reply_to_message.video:
-        return await message.reply_text(
-            "__**You can only save text or stickers in filters.**__"
-        )
-    name = message.text.split(None, 1)[1].strip()
-    if not name:
-        return await message.reply_text("**Usage:**\n__/filter [FILTER_NAME]__")
-    chat_id = message.chat.id
-    if message.reply_to_message.video:
-        
-        _type = "video"
+    try:
+        if len(message.command) < 2:
+            return await message.reply_text(
+                "**Usage:**\nReply to a message with /filter [FILTER_NAME] [CONTENT] To set a new filter."
+            )
+        replied_message = message.reply_to_message
+        if not replied_message:
+            replied_message = message
+        data, name = await get_data_and_name(replied_message, message)
+        if data == "error":
+            return await message.reply_text(
+                "**Usage:**\n__/filter [FILTER_NAME] [CONTENT]__\n`-----------OR-----------`\nReply to a message with.\n/filter [FILTER_NAME]."
+            )
+        if replied_message.text:
+            _type = "text"
+            file_id = None
+        if replied_message.sticker:
+            _type = "sticker"
+            file_id = replied_message.sticker.file_id
+        if replied_message.animation:
+            _type = "animation"
+            file_id = replied_message.animation.file_id
+        if replied_message.photo:
+            _type = "photo"
+            file_id = replied_message.photo.file_id
+        if replied_message.document:
+            _type = "document"
+            file_id = replied_message.document.file_id
+        if replied_message.video:
+            _type = "video"
+            file_id = replied_message.video.file_id
+        if replied_message.video_note:
+            _type = "video_note"
+            file_id = replied_message.video_note.file_id
+        if replied_message.audio:
+            _type = "audio"
+            file_id = replied_message.audio.file_id
+        if replied_message.voice:
+            _type = "voice"
+            file_id = replied_message.voice.file_id
+        if replied_message.reply_markup and not re.findall(r"\[.+\,.+\]", data):
+            urls = extract_urls(replied_message.reply_markup)
+            if urls:
+                response = "\n".join(
+                    [f"{name}=[{text}, {url}]" for name, text, url in urls]
+                )
+                data = data + response
+        if data:
+            data = await check_format(ikb, data)
+            if not data:
+                return await message.reply_text(
+                    "**Wrong formatting, check the help section.**"
+                )
+        name = name.replace("_", " ")
         _filter = {
-        "type": _type,
-        "data": message.reply_to_message.video.file_id   
-        "caption": message.reply_to_message.caption, }
-    else:
-        _type = "text"
-        _filter = {
-        "type": _type,
-        "data": message.reply_to_message.text, }
-
-
-    await save_filter(chat_id, name, _filter)
-    await message.reply_text(f"__**Saved filter {name}.**__")
+            "type": _type,
+            "data": data,
+            "file_id": file_id,
+        }
+        chat_id = message.chat.id
+        await save_filter(chat_id, name, _filter)
+        return await message.reply_text(f"__**Saved filter {name}.**__")
+    except UnboundLocalError:
+        return await message.reply_text(
+            "**Replied message is inaccessible.\n`Forward the message and try again`**"
+        )
 
 
 @app.on_message(filters.command("filters") & ~filters.private)
@@ -96,7 +153,7 @@ async def get_filterss(_, message):
     await message.reply_text(msg)
 
 
-@app.on_message(filters.command("sto") & ~filters.private)
+@app.on_message(filters.command("stop") & ~filters.private)
 @adminsOnly("can_change_info")
 async def del_filter(_, message):
     if len(message.command) < 2:
@@ -113,11 +170,14 @@ async def del_filter(_, message):
 
 
 @app.on_message(
-    filters.text & ~filters.private & ~filters.via_bot & ~filters.forwarded,
+    filters.text & ~filters.private & ~filters.channel & ~filters.via_bot & ~filters.forwarded,
     group=chat_filters_group,
 )
 @capture_err
 async def filters_re(_, message):
+    from_user = message.from_user if message.from_user else message.sender_chat
+    user_id = from_user.id
+    chat_id = message.chat.id
     text = message.text.lower().strip()
     if not text:
         return
@@ -129,36 +189,126 @@ async def filters_re(_, message):
             _filter = await get_filter(chat_id, word)
             data_type = _filter["type"]
             data = _filter["data"]
-            if data_type == "video":
-                caption = _filter["caption"]
-            if data_type == "text":
-                keyb = None
+            file_id = _filter.get("file_id")
+            keyb = None
+            if data:
+                if "{chat}" in data:
+                    data = data.replace(
+                        "{chat}", message.chat.title
+                    )
+                if "{name}" in data:
+                    data = data.replace(
+                        "{name}", (from_user.mention if message.from_user else from_user.title)
+                    )
                 if re.findall(r"\[.+\,.+\]", data):
                     keyboard = extract_text_and_keyb(ikb, data)
                     if keyboard:
                         data, keyb = keyboard
+            replied_message = message.reply_to_message
+            if replied_message:
+                replied_user = replied_message.from_user if replied_message.from_user else replied_message.sender_chat
+                if text.startswith("~"):
+                    await message.delete()
+                if replied_user.id != from_user.id:
+                    message = replied_message
 
-                if message.reply_to_message:
-                    await message.reply_to_message.reply_text(
-                        data,
-                        reply_markup=keyb,
-                        disable_web_page_preview=True,
-                    )
-
-                    if text.startswith("~"):
-                        await message.delete()
-                    return
-
-                return await message.reply_text(
-                    data,
+            if data_type == "text":
+                await message.reply_text(
+                    text=data,
                     reply_markup=keyb,
                     disable_web_page_preview=True,
                 )
-            if message.reply_to_message:
-                await message.reply_to_message.reply_video(data, caption)
-                #await message.reply_to_message.text
+            else:
+                if not file_id:
+                    continue
+            if data_type == "sticker":
+                await message.reply_sticker(
+                    sticker=file_id,
+                )
+            if data_type == "animation":
+                await message.reply_animation(
+                    animation=file_id,
+                    caption=data,
+                    reply_markup=keyb,
+                )
+            if data_type == "photo":
+                await message.reply_photo(
+                    photo=file_id,
+                    caption=data,
+                    reply_markup=keyb,
+                )
+            if data_type == "document":
+                await message.reply_document(
+                    document=file_id,
+                    caption=data,
+                    reply_markup=keyb,
+                )
+            if data_type == "video":
+                await message.reply_video(
+                    video=file_id,
+                    caption=data,
+                    reply_markup=keyb,
+                )
+            if data_type == "video_note":
+                await message.reply_video_note(
+                    video_note=file_id,
+                )
+            if data_type == "audio":
+                await message.reply_audio(
+                    audio=file_id,
+                    caption=data,
+                    reply_markup=keyb,
+                )
+            if data_type == "voice":
+                await message.reply_voice(
+                    voice=file_id,
+                    caption=data,
+                    reply_markup=keyb,
+                )
+            return  # NOTE: Avoid filter spam
 
-                if text.startswith("~"):
-                    await message.delete()
-                return
-            return await message.reply_video(data, caption)
+
+@app.on_message(filters.command("stopall") & ~filters.private)
+@adminsOnly("can_change_info")
+async def stop_all(_, message):
+    _filters = await get_filters_names(message.chat.id)
+    if not _filters:
+        await message.reply_text("**No filters in this chat.**")
+    else:
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "YES, DO IT", callback_data="stop_yes"
+                    ),
+                    InlineKeyboardButton("Cancel", callback_data="stop_no"),
+                ]
+            ]
+        )
+        await message.reply_text(
+            "**Are you sure you want to delete all the filters in this chat forever ?.**",
+            reply_markup=keyboard,
+        )
+
+
+@app.on_callback_query(filters.regex("stop_(.*)"))
+async def stop_all_cb(_, cb):
+    chat_id = cb.message.chat.id
+    from_user = cb.from_user
+    permissions = await member_permissions(chat_id, from_user.id)
+    permission = "can_change_info"
+    if permission not in permissions:
+        return await cb.answer(
+            f"You don't have the required permission.\n Permission: {permission}",
+            show_alert=True,
+        )
+    input = cb.data.split("_", 1)[1]
+    if input == "yes":
+        stoped_all = await deleteall_filters(chat_id)
+        if stoped_all:
+            return await cb.message.edit(
+                "**Successfully deleted all filters on this chat.**"
+            )
+    if input == "no":
+        await cb.message.reply_to_message.delete()
+        await cb.message.delete()
