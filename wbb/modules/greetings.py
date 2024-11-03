c697e308f8d21bd496b3369284e2a71a502bf0db
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-11-04 05:26:17 +0700

    Update greetings.py

--- wbb/modules/greetings.py
+++ wbb/modules/greetings.py
@@ -127,17 +127,6 @@ async def handle_new_member(member, chat):
     try:
         if member.id in SUDOERS:
             return  # Ignore sudo users
-        fed_id = await get_fed_id(chat.id)
-        if fed_id:
-            check_user = await check_banned_user(fed_id, member.id)
-            if check_user:
-                reason = check_user["reason"]
-                date = check_user["date"]
-                await chat.ban_member(member.id)
-                return await app.send_message(
-                    chat.id,
-                    f"**User {member.mention} was Fed Banned.\n\nReason: {reason}.\nDate: {date}.**",
-                )
         if await is_gbanned_user(member.id):
             await chat.ban_member(member.id)
             await app.send_message(
