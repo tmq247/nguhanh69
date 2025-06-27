ac6b9bed1ea1cf5bd51d82724fa50960faa6a874
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-27 18:30:59 +0700

    Update admin2.py

--- wbb/modules/admin2.py
+++ wbb/modules/admin2.py
@@ -126,7 +126,7 @@ async def url_bio(_, message):
 
     if not bio or not user:
         return
-    mods = await list_admins(chat_id)
+    mods = await admins(chat_id)
     if user.id in mods or user.id in SUDOERS:
         return
 
